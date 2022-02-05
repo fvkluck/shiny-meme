75405c9488190a5dea573c6837176ae4c3fd9fa0
FlorianGit <f.v.kluck@gmail.com>
2022-02-05 08:49:20 +0100

    First PoC sending a local notification

--- src/reminder/core.cljs
+++ src/reminder/core.cljs
@@ -1,9 +1,14 @@
 (ns reminder.core
-  (:require [reagent.core :as r]
+  (:require [cljs.core.async :refer [go]]
+            [cljs.core.async.interop :refer [<p!]]
+            [reagent.core :as r]
             [reagent.react-native :as rn]
-            [tick.core :as t]))
+            [tick.core :as t]
+            ["@notifee/react-native" :as notifee :refer [default]]))
 
 (def title-style {:font-size 50})
+
+(def n (aget notifee "default"))
 
 (defonce state (r/atom {:cue-time "08:30" 
                         :reminder-time "17:00"
@@ -13,8 +18,9 @@
 
 (defn logger [x]
   (do
-  (doseq [k (js-keys x)]
-    (do (js/console.log k) (js/console.log (aget x k))))
+    (js/console.log "start logger")
+    (doseq [k (js-keys x)]
+      (do (js/console.log k) (js/console.log (aget x k))))
   x))
 
 (defn new-id [goals]
@@ -26,6 +32,14 @@
 (defn add-goal [day text]
   (let [id (new-id (:goals @day))]
     (swap! day update :goals conj [id {:text text}])))
+
+(defn send-notification
+  ([title body] (send-notification title body (:channel-name @state)))
+  ([title body channel-name]
+   (go
+     <p! (.displayNotification n #js {"title" title
+                                      "body" body
+                                      "android" #js {"channelId" channel-name}}))))
 
 (defn cue-view [day]
   (let [new-goal-text (r/atom "")]
@@ -42,11 +56,15 @@
                                         (add-goal day @new-goal-text)
                                         (.focus (-> % .-target))
                                         (.clear (-> % .-target)))}]
-     [rn/button {:on-press #(add-goal day @new-goal-text) :title "save"}]]))
+     [rn/button {:on-press #(add-goal day @new-goal-text) :title "save"}]
+     [rn/button {:on-press #(send-notification "Dit is een titel" "dit is een body") :title "Send notification"}]]))
 
 (defn hello []
   [rn/view  {:style  {:flex 1 :align-items  "center" :justify-content  "center"}}
    [cue-view (r/cursor state [:days 0])]])
 
 (defn ^:export -main  [& args]
+  (go
+    (let [c (<p! (.createChannel n #js {:id "reminder-channel" :name "channel for reminder app"}))]
+      (swap! state assoc :channel-name c)))
   (r/as-element  [hello]))
