e7cb29da2682e7eaafafa454f0ec3e1b96f9591f
FlorianGit <f.v.kluck@gmail.com>
2022-02-23 11:17:40 +0100

    Working version

--- src/reminder/core.cljs
+++ src/reminder/core.cljs
@@ -35,38 +35,76 @@
   (let [id (new-id (:goals @day))]
     (swap! day update :goals conj [id {:text text}])))
 
-(defn send-notification
-  ([title body] (send-notification title body (:channel-name @state)))
-  ([title body channel-name]
-   (go
-     <p! (.displayNotification n #js {"title" title
-                                      "body" body
-                                      "android" #js {"channelId" channel-name}}))))
+(defn send-notification [title body & {:keys [channel-name trigger-time]
+                                       :or {channel-name (:channel-name @state)
+                                            trigger-time (t/>> (t/now) (t/new-duration 2 :seconds))}}]
+  (go
+    <p! (n/createTriggerNotification #js {"title" title
+                                          "body" body
+                                          "android" #js {"channelId" channel-name}}
+                                     #js {"type" (.-TIMESTAMP TriggerType)
+                                          "timestamp" (.getTime (js/Date. trigger-time))})))
+(comment
+  (defn send-notification
+    ([title body] (send-notification title body (:channel-name @state)))
+    ([title body channel-name] (send-notification title
+                                                  body
+                                                  channel-name
+                                                  (+ (.getTime (js/Date. (.now js/Date))) 2000)))
+    ([title body channel-name trigger-time]
+     (go
+       <p! (n/createTriggerNotification #js {"title" title
+                                             "body" body
+                                             "android" #js {"channelId" channel-name}}
+                                        #js {"type" (.-TIMESTAMP TriggerType)
+                                             "timestamp" trigger-time})))))
 
-(defn cue-view [day]
+(comment 
+  (def date (js/Date. (.now js/Date)))
+  (.setMinutes date 11)
+  (send-notification "title" "body" (:channel-name @state) (.getTime date)))
+
+(defn cue-view []
+  (let [day (get-in state [:days 0])
+        new-goal-text (r/atom "")]
+    (r/as-element [rn/view {:style {:flex 1 :align-items "center" :justify-content "center"}}
+                   [rn/text {:style title-style} "Set your goals"]
+                   [rn/text "Please take a moment to consider what you want to achieve today."]
+                   (for [[g-id g] (:goals @day)]
+                     [rn/view {:key g-id :flex-direction "row" :margin-bottom 10}
+                      [rn/text-input {:key g-id :default-value (:text g)
+                                      :on-change-text #(swap! day assoc-in [:goals g-id :text] %)}]
+                      [rn/button {:on-press #(swap! day update :goals dissoc g-id) :title "Remove"}]])
+                   [rn/text-input {:placeholder "my new goal" :on-change-text #(reset! new-goal-text %)
+                                   :on-end-editing #(do
+                                                      (add-goal day @new-goal-text)
+                                                      (.focus (-> % .-target))
+                                                      (.clear (-> % .-target)))}]
+                   [rn/button {:on-press #(send-notification "Dit is een titel" "dit is een body") :title "Remind me tonight!"}]])))
+
+(defn reminder-view [day]
   (let [new-goal-text (r/atom "")]
     [rn/view {:style {:flex 1 :align-items "center" :justify-content "center"}}
-     [rn/text {:style title-style} "Set your goals"]
-     [rn/text "Please take a moment to consider what you want to achieve today."]
-     (for [[g-id g] (:goals @day)]
-       [rn/view {:key g-id :flex-direction "row" :margin-bottom 10}
-        [rn/text-input {:key g-id :default-value (:text g)
-                        :on-change-text #(swap! day assoc-in [:goals g-id :text] %)}]
-        [rn/button {:on-press #(swap! day update :goals dissoc g-id) :title "Remove"}]])
-     [rn/text-input {:placeholder "my new goal" :on-change-text #(reset! new-goal-text %)
-                     :on-end-editing #(do
-                                        (add-goal day @new-goal-text)
-                                        (.focus (-> % .-target))
-                                        (.clear (-> % .-target)))}]
-     [rn/button {:on-press #(add-goal day @new-goal-text) :title "save"}]
-     [rn/button {:on-press #(send-notification "Dit is een titel" "dit is een body") :title "Send notification"}]]))
+                   [rn/text {:style title-style} "Your plan"]
+                   [rn/text "For better or for worse this is what you had planned"]
+                   (for [[g-id g] (:goals @day)]
+                     [rn/view {:key g-id :flex-direction "row" :margin-bottom 10}
+                      [rn/text {:key g-id
+                                :style {:color (if (:finished g) "#009900" "#ff0000")}} (:text g)]
+                      [rn/button {:on-press #(swap! day update-in [:goals g-id] assoc :finished true) :title "Finished"}]])
+                   [rn/text-input {:placeholder "my new goal" :on-change-text #(reset! new-goal-text %)
+                                   :on-end-editing #(do
+                                                      (add-goal day @new-goal-text)
+                                                      (.focus (-> % .-target))
+                                                      (.clear (-> % .-target)))}]]))
 
 (defn hello []
-  [rn/view  {:style  {:flex 1 :align-items  "center" :justify-content  "center"}}
-   [cue-view (r/cursor state [:days 0])]])
+  [:> NavigationContainer {}
+    [rn/view  {:style  {:flex 1 :align-items  "center" :justify-content  "center"}}
+     [reminder-view (r/cursor state [:days 0])]]])
 
 (defn ^:export -main  [& args]
   (go
-    (let [c (<p! (.createChannel n #js {:id "reminder-channel" :name "channel for reminder app"}))]
+    (let [c (<p! (n/createChannel #js {:id "reminder-channel" :name "channel for reminder app"}))]
       (swap! state assoc :channel-name c)))
-  (r/as-element  [hello]))
+  (r/as-element [hello]))
