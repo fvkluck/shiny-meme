dd53fe28c6723518171d6bb1996ef4ac3e77e642
FlorianGit <f.v.kluck@gmail.com>
2022-03-02 14:16:07 +0100

    Clean up a bit

--- src/reminder/core.cljs
+++ src/reminder/core.cljs
@@ -44,47 +44,27 @@
                                           "android" #js {"channelId" channel-name}}
                                      #js {"type" (.-TIMESTAMP TriggerType)
                                           "timestamp" (.getTime (js/Date. trigger-time))})))
-(comment
-  (defn send-notification
-    ([title body] (send-notification title body (:channel-name @state)))
-    ([title body channel-name] (send-notification title
-                                                  body
-                                                  channel-name
-                                                  (+ (.getTime (js/Date. (.now js/Date))) 2000)))
-    ([title body channel-name trigger-time]
-     (go
-       <p! (n/createTriggerNotification #js {"title" title
-                                             "body" body
-                                             "android" #js {"channelId" channel-name}}
-                                        #js {"type" (.-TIMESTAMP TriggerType)
-                                             "timestamp" trigger-time})))))
-
-(comment 
-  (def date (js/Date. (.now js/Date)))
-  (.setMinutes date 11)
-  (send-notification "title" "body" (:channel-name @state) (.getTime date)))
 
 (defn cue-view [{:keys [navigation]}]
   (let [day (r/cursor state [:days 0])
         new-goal-text (r/atom "")]
-    (r/as-element [rn/view {:style {:flex 1 :align-items "center" :justify-content "center"}}
-                   [rn/text {:style title-style} "Set your goals"]
-                   [rn/text "Please take a moment to consider what you want to achieve today."]
-                   (for [[g-id g] (:goals @day)]
-                     [rn/view {:key g-id :flex-direction "row" :margin-bottom 10}
-                      [rn/text-input {:key g-id :default-value (:text g)
-                                      :on-change-text #(swap! day assoc-in [:goals g-id :text] %)}]
-                      [rn/button {:on-press #(swap! day update :goals dissoc g-id) :title "Remove"}]])
-                   [rn/text-input {:placeholder "my new goal" :on-change-text #(reset! new-goal-text %)
-                                   :on-end-editing #(do
-                                                      (add-goal day @new-goal-text)
-                                                      (.focus (-> % .-target))
-                                                      (.clear (-> % .-target)))}]
-                   [rn/button {:on-press #(send-notification "Dit is een titel" "dit is een body") :title "Remind me tonight!"}]])))
+    [rn/view {:style {:flex 1 :align-items "center" :justify-content "center"}}
+     [rn/text {:style title-style} "Set your goals"]
+     [rn/text "Please take a moment to consider what you want to achieve today."]
+     (for [[g-id g] (:goals @day)]
+       [rn/view {:key g-id :flex-direction "row" :margin-bottom 10}
+        [rn/text-input {:key g-id :default-value (:text g)
+                        :on-change-text #(swap! day assoc-in [:goals g-id :text] %)}]
+        [rn/button {:on-press #(swap! day update :goals dissoc g-id) :title "Remove"}]])
+     [rn/text-input {:placeholder "my new goal" :on-change-text #(reset! new-goal-text %)
+                     :on-end-editing #(do
+                                        (add-goal day @new-goal-text)
+                                        (.focus (-> % .-target))
+                                        (.clear (-> % .-target)))}]
+     [rn/button {:on-press #(send-notification "Dit is een titel" "dit is een body") :title "Remind me tonight!"}]]))
 
 (defn reminder-view [{:keys [navigation]}]
-  (let [new-goal-text (r/atom "")
-        day (r/cursor state [:days 0])]
+  (let [day (r/cursor state [:days 0])]
     [rn/view {:style {:flex 1 :align-items "center" :justify-content "center"}}
                    [rn/text {:style title-style} "Your plan"]
                    [rn/text "For better or for worse this is what you had planned"]
@@ -92,12 +72,8 @@
                      [rn/view {:key g-id :flex-direction "row" :margin-bottom 10}
                       [rn/text {:key g-id
                                 :style {:color (if (:finished g) "#009900" "#ff0000")}} (:text g)]
-                      [rn/button {:on-press #(swap! day update-in [:goals g-id] assoc :finished true) :title "Finished"}]])
-                   [rn/text-input {:placeholder "my new goal" :on-change-text #(reset! new-goal-text %)
-                                   :on-end-editing #(do
-                                                      (add-goal day @new-goal-text)
-                                                      (.focus (-> % .-target))
-                                                      (.clear (-> % .-target)))}]]))
+                      [rn/button {:on-press #(swap! day update-in [:goals g-id] assoc :finished true) :title "Finished"}]])]))
+
 (defn home [{:keys [navigation]}]
   [rn/view {:style {:flex 1 :align-items "center" :justify-content "center"}}
    [rn/button {:on-press #(.navigate navigation "cue") :title "go to cue-view"}]
@@ -105,7 +81,7 @@
 
 (defn app []
   [:> NavigationContainer {}
-   [:> (.-Navigator Stack) {:screen-options {:title "Awesome Project"
+   [:> (.-Navigator Stack) {:screen-options {:title "Welcome to Reminder"
                                              :header-style {:background-color "green" :height 100}
                                              :header-tint-color "#fff"
                                              :header-title-style {:font-size 30 :text-align "center"}}}
